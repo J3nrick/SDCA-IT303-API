7461d2ff4d296092e7ffb431f0dc0710530e88d0
Jenrick Ambalong <jenerick.101@gmail.com>
2025-10-11 16:16:32 +0800

    enable cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 import { userRoutes } from './routes/user.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/users' , userRoutes)
 
@@ -13,6 +15,6 @@ app.use('/api/users' , userRoutes)
 mongoose.connect(process.env.MONGODB_URL)
 mongoose.connection.once('open', () => console.log('Now connected to MongDB Atlas,'))
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000.')
 })
